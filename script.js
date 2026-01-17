f8e541a6ee15939a4e70518283fe00a081e243fd
Akash <theugliestking000@gmail.com>
2026-01-17 17:42:08 +0530

    Update website + others animation

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+/*!
+Copyright (c) 2026 Martin Laxenaire
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+*/
+
+
+
 /* ================= Smooth scroll for #cta (if present) ================= */
 const ctaBtn = document.getElementById("cta");
 if (ctaBtn) {
@@ -761,6 +776,7 @@ const isLowEndDevice =
   navigator.hardwareConcurrency <= 4 ||
   navigator.deviceMemory <= 4;
 
+  
 if (!isLowEndDevice) {
 
 // Water overlay setup (Phase 1)
@@ -848,6 +864,9 @@ function drawRipples() {
 
 drawRipples();
 }
+
+
+
 
 const cursor = document.getElementById("custom-cursor");
 
@@ -1003,3 +1022,7 @@ function startBackgroundMusic() {
 // Start music on first interaction
 document.addEventListener("click", startBackgroundMusic, { once: true });
 document.addEventListener("touchstart", startBackgroundMusic, { once: true });
+
+
+/* ---------new ripple overlay------------ */
+
